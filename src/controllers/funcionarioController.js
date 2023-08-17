aee8ecad7fa24333fca15322f15c8d6190529d49
joaolucasgomesnog <lucasgomes145987@gmail.com>
2023-08-16 22:01:59 -0300

    alteração no menu lateral

---
+++ src/controllers/funcionarioController.js
@@ -0,0 +1,145 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export default {
+
+    async createFuncionario(req, res) {
+        const { cpf, nome, rg, datNascimento, num_sus, endereco, contato } = req.body;
+
+        try {
+            const paciente = await prisma.paciente.create({
+                data: {
+                    cpf: cpf,
+                    nome: nome,
+                    rg: rg,
+                    datNascimento: datNascimento,
+                    num_sus: num_sus,
+                    endereco: {
+                        create: {
+                            cep: endereco.cep,
+                            rua: endereco.rua,
+                            numero: endereco.numero,
+                            bairro: endereco.bairro,
+                            cidade: endereco.cidade
+                        }
+                    },
+                    contato: {
+                        create: {
+                            email_1: contato.email_1,
+                            telefone_1: contato.telefone_1
+                        }
+                    },
+                },
+                include: {
+                    endereco: true,
+                    contato: true
+                }
+            });
+
+            res.json(paciente);
+        } catch (error) {
+            console.error('Erro ao criar paciente:', error);
+            res.status(500).json({ error: 'Erro ao criar paciente' });
+        }
+    },
+
+    async findAllPacientes(req, res) {
+        try {
+            const pacientes = await prisma.paciente.findMany()
+            return res.json(pacientes)
+
+        } catch (error) {
+            return res.json({ error })
+        }
+    },
+    async findPacienteById(req, res) {
+        try {
+            const { id } = req.params
+            const paciente = await prisma.paciente.findUnique({ where: { id: Number(id) } })
+            if (!paciente) return res.json({ error: "Usuário não existe" })
+            return res.json(paciente)
+
+        } catch (error) {
+            return res.json({ error })
+        }
+    },
+    async findPacienteByAll(req, res) {
+        try {
+            const { nome } = req.params;
+            const pacientes = await prisma.$queryRaw`
+            SELECT * FROM "Paciente"
+            WHERE LOWER("nome") LIKE ${`%${nome.toLowerCase()}%`}
+            OR "cpf" LIKE ${`%${nome}%`}
+            OR "num_sus" LIKE ${`%${nome}%`}
+            OR "rg" LIKE ${`%${nome}%`};
+            
+        `;
+    
+            if (!pacientes || pacientes.length === 0) {
+                return res.json({ error: "Usuário não existe" });
+            }
+    
+            return res.json(pacientes);
+        } catch (error) {
+            console.error("Erro ao buscar pacientes:", error);
+            return res.status(500).json({ error: "Ocorreu um erro ao buscar pacientes." });
+        }
+    },
+    async updatePaciente(req, res) {
+        try {
+            const { id } = req.params
+
+            const { cpf, nome, rg, datNascimento, num_sus, endereco, contato } = req.body;
+            const { cep, rua, numero, bairro, cidade } = endereco;
+            const { email_1, telefone_1 } = contato;
+
+            let paciente = await prisma.paciente.findUnique({ where: { id: Number(id) } })
+            if (!paciente)
+                return res.status(404).json({ error: "Usuário não existe" })
+
+                paciente = await prisma.paciente.update({ 
+                    where: { id: Number(id) }, 
+                    data: { 
+                        cpf, 
+                        nome, 
+                        rg, 
+                        datNascimento, 
+                        num_sus, 
+                        endereco: {
+                            update: {
+                                cep,
+                                rua,
+                                numero,
+                                bairro,
+                                cidade
+                            }
+                        },
+                        contato: {
+                            update: {
+                                email_1,
+                                telefone_1
+                            }
+                        }
+                    } 
+                });
+            return res.json(paciente)
+
+        } catch (error) { 
+            console.error(error)
+            return res.status(500).json({ error: "Não foi possivel atualizar o paciente" }) }
+    },
+
+
+    async deletePacienteById(req, res) {
+        try {
+            const { id } = req.params
+            let paciente = await prisma.paciente.findUnique({ where: { id: Number(id) } })
+            if (!paciente) return res.json({ error: "Usuário não existe" })
+            await prisma.paciente.delete({ where: { id: Number(id) } })
+            return res.json({message: "Paciente deletado"})
+        } catch (error) {
+            return res.json({ error })
+        }
+    },
+}
--- src/dashboard/sidenavigation/items.jsx
+++ src/dashboard/sidenavigation/items.jsx
@@ -4,8 +4,6 @@ import { useRouter } from 'next/router';
 import data from './data';
 import { useToggle } from '../provider/context';
 
-// Import para signout
-import { auth } from '../../services/firebaseConfig';
 
 
 const style = {
@@ -20,21 +18,13 @@ export default function SidenavItems() {
   const { asPath } = useRouter();
   const { open } = useToggle();
 
-  const router = useRouter();
-
-  async function handleLinkClick(index) {
-    if (data[index].title === 'Sair') {
-      await auth.signOut();
-      router.push('/login');
-    }
-  }
 
   return (
     <ul className="md:pl-3">
       <li>
-        {data.map((item, index) => (
+        {data.map((item) => (
           <Link href={item.link} key={item.title}>
-            <a className={style.link} onClick={() => handleLinkClick(index)}>
+            <a className={style.link}>
               <div
                 className={`p-2 ${item.link === asPath ? style.active : ''}`}
               >
