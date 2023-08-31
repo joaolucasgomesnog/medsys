175d9b941e27c58a23b6ec05febd45cdc67d43ab
georginho1 <supergeorginhox@hotmail.com>
2023-08-30 21:59:26 -0300

    MDS-7 CRUD de funcionarios finalizado

--- src/controllers/funcionarioController.js
+++ src/controllers/funcionarioController.js
@@ -4,93 +4,16 @@ const prisma = new PrismaClient()
 
 export default {
 
-    async createAtendente(req, res) {
-        const { nome, cpf, rg, endereco, contato } = req.body;
-
-        try {
-            const atendente = await prisma.atendente.create({
-                data: {
-                    nome: nome,
-                    cpf: cpf,
-                    rg: rg,
-                    endereco: {
-                        create: {
-                            cep: endereco.cep,
-                            rua: endereco.rua,
-                            numero: endereco.numero,
-                            bairro: endereco.bairro,
-                            cidade: endereco.cidade
-                        }
-                    },
-                    contato: {
-                        create: {
-                            email_1: contato.email_1,
-                            telefone_1: contato.telefone_1
-                        }
-                    },
-                },
-                include: {
-                    endereco: true,
-                    contato: true
-                }
-            });
-
-            res.json(atendente);
-        } catch (error) {
-            console.error('Erro ao criar atendente:', error);
-            res.status(500).json({ error: 'Erro ao criar atendente' });
-        }
-    },
-
-    async createEnfermeira(req, res) {
-        const { coren, nome, cpf, rg, datNascimento, endereco, contato } = req.body;
-
-        try {
-            const enfermeira = await prisma.enfermeira.create({
-                data: {
-                    coren: coren,
-                    nome: nome,
-                    cpf: cpf,
-                    rg: rg,
-                    datNascimento: datNascimento,
-                    endereco: {
-                        create: {
-                            cep: endereco.cep,
-                            rua: endereco.rua,
-                            numero: endereco.numero,
-                            bairro: endereco.bairro,
-                            cidade: endereco.cidade
-                        }
-                    },
-                    contato: {
-                        create: {
-                            email_1: contato.email_1,
-                            telefone_1: contato.telefone_1
-                        }
-                    },
-                },
-                include: {
-                    endereco: true,
-                    contato: true
-                }
-            });
-
-            res.json(enfermeira);
-        } catch (error) {
-            console.error('Erro ao criar enfermeira:', error);
-            res.status(500).json({ error: 'Erro ao criar enfermeira' });
-        }
-    },
-
-
     async findAllFuncionarios(req, res) {
         try {
             const atendentes = await prisma.atendente.findMany()
             const enfemeiros = await prisma.enfermeira.findMany()
-            
+            const medicos = await prisma.medico.findMany()
+
             let retorno = new Object();
             retorno.atendentes = atendentes;
-            retorno.enfermeiros = enfemeiros
+            retorno.enfermeiros = enfemeiros;
+            retorno.medicos = medicos;
             
             return res.json(retorno)
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import PacienteController from "./controllers/PacienteController";
 import funcionarioController from "./controllers/funcionarioController";
+import AtendenteController from "./controllers/atendenteController";
+import EnfermeiraController from "./controllers/EnfermeiraController";
+import MedicoController from "./controllers/MedicoController";
 
 const router = Router()
 
@@ -11,8 +14,30 @@ router.get("/paciente/search/:nome", PacienteController.findPacienteByAll)
 router.put("/paciente/:id", PacienteController.updatePaciente)
 router.delete("/paciente/:id", PacienteController.deletePacienteById)
 
-router.post("/atendente",funcionarioController.createAtendente)
-router.post("/enfermeira",funcionarioController.createEnfermeira)
+router.post("/atendente",AtendenteController.createAtendente)
+router.get("/atendentes",AtendenteController.findAllAtendentes)
+router.get("/atendente/:id",AtendenteController.findAtendenteById)
+router.get("/atendente/search/:nome",AtendenteController.findAtendenteByAll)
+router.put("/atendente/:id",AtendenteController.updateAtendente)
+router.delete("/atendente/:id",AtendenteController.deleteAtendenteById)
+
+
+
+router.post("/enfermeira",EnfermeiraController.createEnfermeira)
+router.get("/enfermeiras",EnfermeiraController.findAllEnfermeiras)
+router.get("/enfermeira/:id",EnfermeiraController.findEnfermeiraById)
+router.get("/enfermeira/search/:nome",EnfermeiraController.findEnfermeiraByAll)
+router.put("/enfermeira/:id",EnfermeiraController.updateEnfermeira)
+router.delete("/enfermeira/:id",EnfermeiraController.deleteEnfermeiraById)
+
+router.post("/medico", MedicoController.createMedico);
+router.get("/medicos", MedicoController.findAllMedicos);
+router.get("/medico/:id", MedicoController.findMedicoById);
+router.get("/medico/search/:nome", MedicoController.findMedicoByAll);
+router.put("/medico/:id", MedicoController.updateMedico);
+router.delete("/medico/:id", MedicoController.deleteMedicoById);
+
+
 router.get("/funcionarios", funcionarioController.findAllFuncionarios)
 
 
